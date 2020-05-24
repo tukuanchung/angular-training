17769e15f76783f0f18c81ad17fa5398ad79ccf4
tukuanchung <tukuanchung@gmail.com>
2020-05-24 21:47:13 +0800

    sales-person-list

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 ng new my-first-angular-project
 ng serve
+
+
+ng new sales-project
+ng generate component sales-person-list
+ng generate class sales-person-list/SalesPerson
---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+  salesPersonList : SalesPerson[] = [
+    new SalesPerson("Anup","kumar", "aaa@lub2code.com", 5000),
+    new SalesPerson("John","Dow","jogndow@gmail.com", 4000),
+    new SalesPerson("Clair","Uan","Clair.uan@gmail.com", 40000),
+    new SalesPerson("Mai","Truong", "mai.truong@luv2code.com", 60000)
+  ];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
